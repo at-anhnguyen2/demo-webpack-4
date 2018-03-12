b5d55e412573232eef613d2fac20943f8f0d61b9
Anh Nguyen T <anh.nguyen2@asiantech.vn>
2018-03-12 12:01:41 +0700

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,170 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname);
+
+const parts = require('./webpack.parts');
+
+/** 
+  * Common Config
+  *
+  * Description: 
+  * - Output
+  * - JavaScript loader
+  */
+const commonConfig = merge([
+  {
+    output: {
+      path: BUILD_DIR,
+      publicPath: '/',
+    },
+  },
+  parts.loadJavaScript({ include: APP_DIR }),
+]);
+
+/** 
+  * Production Config
+  *
+  * Description: 
+  * - Performance
+  * - Build records
+  * - Clean build folder before re-build
+  * - StyleSheet loader
+  * - File loader
+  * - Minify JavaScripts and StyleSheets
+  * - Generation source map
+  * - Optimization chunks
+  */
+const productionConfig = merge([
+  // Performance
+  {
+    performance: {
+      hints: 'warning', // 'error' or false are valid too
+      maxEntrypointSize: 150000, // in bytes, default 250k
+      maxAssetSize: 450000, // in bytes
+    },
+  },
+
+  // Build records
+  {
+    recordsPath: path.join(__dirname, "records.json"),
+    output: {
+      chunkFilename: "[name].[chunkhash:8].js",
+      filename: "[name].[chunkhash:8].js",
+    },
+    plugins: [new webpack.NamedModulesPlugin()],
+  },
+
+  // Clean build folder before re-build
+  parts.clean(BUILD_DIR),
+
+  // Loading and Extract StyleSheets
+  parts.extractCSS({
+    use: [{
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+        sourceMap: true
+      }
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true
+      }
+    }],
+  }),
+
+  // Loading Files
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: '[name].[hash:8].[ext]',
+    },
+  }),
+
+  // Minify JavaScripts and StyleSheets
+  parts.minifyJavaScript(),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+      // Run cssnano in safe mode to avoid
+      // potentially unsafe transformations.
+      safe: true,
+    },
+  }),
+
+  // Generation source map
+  parts.generateSourceMaps({ type: 'source-map' }),
+
+  // Optimization Chunks
+  {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
+        },
+      },
+      runtimeChunk: {
+        name: 'manifest',
+      },
+    },
+  },
+]);
+
+/** 
+  * Devalopment Config
+  *
+  * Description: 
+  * - Create development server
+  * - StyleSheet loader
+  * - File loader
+  */
+const developmentConfig = merge([
+  // Development server
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+
+  // Loading StyleSheets
+  parts.loadCSS(),
+
+  // Loading Files
+  parts.loadImages(),
+]);
+
+/** 
+  * Exports Config Function
+  *
+  * @param: env  (environment of config)
+  * @param: argv (array option on CLI)
+  *
+  * Description: 
+  * - Determine pages
+  * - Determine config mode
+  * - Return config to exports
+  */
+module.exports = (env, argv) => {
+  const pages = [
+    parts.page({
+      title: 'Webpack demo',
+      entry: {
+        app: path.join(APP_DIR, "index.js"),
+      },
+      chunks: ['app', 'manifest', 'vendor'],
+    })
+  ];
+  const config = argv.mode === 'production' ? productionConfig : developmentConfig;
+
+  return merge([commonConfig, config, { mode: argv.mode }].concat(pages));
+};
---
+++ webpack.parts.js
@@ -0,0 +1,216 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssnano = require('cssnano');
+
+/** 
+  * Multiple page
+  *
+  * @param: path    (the path include index.html, defaul = string empty)
+  * @param: title   (the title of page, allowed NULL)
+  * @param: entry   (the entries of page, not NULL)
+  * @param: chunks  (this chunks of page, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development and Production
+  */
+exports.page = ({ path = '', title, entry, chunks,} = {}) => ({
+  entry,
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks,
+      filename: `${path && path + '/'}index.html`,
+      title,
+    }),
+  ],
+});
+
+/** 
+  * Development Server
+  *
+  * @param: host  (the host of development server, allowed NULL)
+  * @param: port  (the port of development server, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development
+  */
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: 'errors-only',
+    host, // Default: localhost
+    port, // Default: 8080
+    open: true,
+    progress: true,
+    inline: true,
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+
+/** 
+  * Development debug tool
+  *
+  * @param: type  (the type of development debug tool, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development and Production
+  */
+exports.generateSourceMaps = ({ type }) => ({
+  devtool: type, // Default: 'eval' for development
+});
+
+/** 
+  * Loading JavaScripts
+  *
+  * @param: include (the folders are loaded, allowed NULL)
+  * @param: exclude (the folders are not loaded, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development and Production
+  */
+exports.loadJavaScript = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include,
+        exclude,
+        use: 'babel-loader',
+      },
+    ],
+  },
+});
+
+/** 
+  * Extract and Loading StyleSheet
+  *
+  * @param: include (the folders are loaded, allowed NULL)
+  * @param: exclude (the folders are not loaded, allowed NULL)
+  * @param: use     (the loaders are used, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Description: Output extracted CSS to a file
+  * - Use: Production
+  */
+exports.extractCSS = ({ include, exclude, use }) => {
+  const plugin = new ExtractTextPlugin({
+    // allChunks is needed to extract from extracted chunks as well.
+    allChunks: true,
+    filename: '[name].[contenthash:8].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [plugin],
+  };
+};
+
+/** 
+  * Loading StyleSheet
+  *
+  * @param: include (the folders are loaded, allowed NULL)
+  * @param: exclude (the folders are not loaded, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development
+  */
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        include,
+        exclude,
+        use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
+      },
+    ],
+  },
+});
+
+/** 
+  * Loading Another file
+  *
+  * @param: include (the folders are loaded, allowed NULL)
+  * @param: exclude (the folders are not loaded, allowed NULL)
+  * @param: options (the options are queried when loading, allowed NULL)
+  *
+  * Member of Webpack parts
+  * - Use: Development and Production
+  */
+exports.loadImages = ({ include, exclude, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|svg)$/,
+        include,
+        exclude,
+        use: {
+          loader: 'url-loader',
+          options,
+        },
+      },
+    ],
+  },
+});
+
+/** 
+  * Minify JavaScript
+  *
+  * Member of Webpack parts
+  * - Use: Production
+  */
+exports.minifyJavaScript = () => ({
+  optimization: {
+    minimizer: [new UglifyWebpackPlugin()],
+  },
+});
+
+/** 
+  * Minify StyleSheet
+  *
+  * @param: options (the options are queried when minifying, allowed NULL)
+  *
+  * Member of Webpack parts
+  * Use: Production
+  */
+exports.minifyCSS = ({ options }) => ({
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: options,
+      canPrint: false,
+    }),
+  ],
+});
+
+/** 
+  * Clean Build Folder
+  *
+  * @param: path (the path of build folder, not allowed NULL)
+  *
+  * Member of Webpack parts
+  * Use: Production
+  */
+exports.clean = path => ({
+  plugins: [new CleanWebpackPlugin([path])],
+});
